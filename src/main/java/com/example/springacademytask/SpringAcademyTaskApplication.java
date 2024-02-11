0db60fb4b18f48324e6262c6d448df8ea06e9012
Ekaterina Krivokoneva <katya.krivokoneva@gmail.com>
2024-02-11 17:56:00 +0400

    init: start task

---
+++ src/main/java/com/example/springacademytask/SpringAcademyTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.springacademytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademyTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademyTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springacademytask/SpringAcademyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springacademytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademyTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
